c9c423b73b0b787921ee6c678beb2cd3a80fd061
Sharleen-Kariuki <sharleenwambui28@gmail.com>
2025-01-08 23:18:15 +0300

    general use ecommerce website frontend

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>  
+        <div className="descriptionbox-description">
+            <p>
+            The program is offered through a hybrid model (online and in-person sessions). The 9 months of the program is inclusive of program breaks while the training itself takes 8 months. 
+            </p>
+            <p>
+            The program is offered through a hybrid model (online and in-person sessions). The 9 months of the program is inclusive of program breaks while the training itself takes 8 months. 
+            </p>
+        </div> 
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
